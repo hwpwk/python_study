92d24de8f4532b8c1b6852ec80d6047f834b64cb
k.tomori <k.tomori@datumstudio.jp>
2018-10-29 19:01:47 +0900

    last commit

--- クラスの初期化とクラス変数_7_78.py
+++ クラスの初期化とクラス変数_7_78.py
@@ -31,7 +31,7 @@ class Person(object):
     def say_something(self):
         print('I am {}. hello'.format(self.name))
 
-person = Person('Tom')# 1つ下のperson.say_something()がなくてもdef__init__(self)を実行できる
+person = Person('Tom')
 person.say_something()
 
 #####################################################
@@ -48,5 +48,21 @@ class Person(object):
     def run(self):
         print('run')
 
-person = Person('Tom')# 1つ下のperson.say_something()がなくてもdef__init__(self)を実行できる
+person = Person('Tom')
+person.say_something()
+#####################################################
+#####################################################
+# run()に引数numを追加 say_something()のself.run()に引数を追加
+class Person(object):
+    def __init__(self, name):
+        self.name = name# self(自分自身)のnameに引数を入れる
+
+    def say_something(self):
+        print('I am {}. hello'.format(self.name))
+        self.run(10)
+
+    def run(self, num):
+        print('run'* num)
+
+person = Person('Tom')
 person.say_something()
