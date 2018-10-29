567c0ded579f3d54379377a710750a361e39afab
k.tomori <k.tomori@datumstudio.jp>
2018-10-29 20:06:07 +0900

    def __del__(self)が最後に実行される

--- コンストラクタとデストラクタ_7_79.py
+++ コンストラクタとデストラクタ_7_79.py
@@ -15,3 +15,25 @@ class Person(object):
 
 person = Person('Tom')
 person.say_something()
+
+#####################################################
+#####################################################
+class Person(object):
+    def __init__(self, name):# この初期化したものをコンストラクタと呼ぶ
+        self.name = name
+
+    def say_something(self):
+        print('I am {}. hello'.format(self.name))
+        self.run(10)
+
+    def run(self, num):
+        print('run'* num)
+# person.say_something()オブジェクトが使われなくなった時点でdef __del__(self)が呼び出される
+    def __del__(self):
+        print('good bye')
+
+person = Person('Tom')
+person.say_something()
+
+print('###################')
+# このコードの場合##########の後にgood bye と表示される
