af58c6000b69b244bdb687473063870fff67d9c8
k.tomori <k.tomori@datumstudio.jp>
2018-10-29 20:10:47 +0900

    del person last commit

--- コンストラクタとデストラクタ_7_79.py
+++ コンストラクタとデストラクタ_7_79.py
@@ -37,3 +37,27 @@ person.say_something()
 
 print('###################')
 # このコードの場合##########の後にgood bye と表示される
+
+#####################################################
+#####################################################
+# ##########の後にgood bye と表示されるのでなく明示的にdelを呼び出したいとき
+class Person(object):
+    def __init__(self, name):# この初期化したものをコンストラクタと呼ぶ
+        self.name = name
+
+    def say_something(self):
+        print('I am {}. hello'.format(self.name))
+        self.run(10)
+
+    def run(self, num):
+        print('run'* num)
+
+    def __del__(self):
+        print('good bye')
+
+person = Person('Tom')
+person.say_something()
+
+del person
+# こうすることでgood byeが######より先に呼び出される
+print('###################')
